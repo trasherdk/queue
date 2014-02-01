3268fff556954987474113ad64a7826cecf10d85
Nathan Zadoks <nathan@nathan7.eu>
2014-02-01 15:59:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# then-queue
+
+  a simple asynchronous queue
+
+## Installation
+
+    npm install then-queue
+
+## API
+### Queue()
+
+  A fresh queue!
+
+### queue.push(item)
+
+  Push an item onto the queue
+
+### queue.pop() -> Promise Item
+
+  Pop an item from the queue
+
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+var Promise = require('promise')
+
+function Queue() {
+  if (!(this instanceof Queue)) return new Queue()
+  this._items = []
+  this._waiting = []
+}
+
+Queue.prototype.push = function(item) {
+  var waiting = this._waiting.pop()
+  if (waiting)
+    waiting(item)
+  else
+    this._items.push(item)
+}
+
+Queue.prototype.pop = function() {
+  var item = this._items.pop()
+  return item
+    ? Promise.from(item)
+    : new Promise(function(resolve, reject) {
+        this._waiting.push(resolve)
+      })
+}
