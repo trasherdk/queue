877faf1662488b0def8328f237be90f7cafb45ec
Forbes Lindesay <forbes@lindesay.co.uk>
2014-04-28 11:17:27 +0100

    Add `length-changed` event.

--- README.md
+++ README.md
@@ -32,3 +32,7 @@ var q = new Queue();
 ### queue.length
 
   Amount of items in the queue (note that this can be negative if `pop` has been called more times than `push`).
+
+### Events
+
+The `length-changed` event gets emitted whenever `pop` or `push` has been called. You could use it to spawn/kill workers when the length changes.
--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
 
+var EventEmitter = require('events').EventEmitter
 var Promise = require('promise')
 
 module.exports = Queue
 function Queue() {
   if (!(this instanceof Queue)) return new Queue()
+  EventEmitter.call(this)
   this._items = []
   this._waiting = []
   this.length = 0
 }
+Queue.prototype = Object.create(EventEmitter.prototype)
+Queue.prototype.constructor = Queue
 
 Queue.prototype.push = function(item) {
   this.length++
+  this.emit('length-changed', this.length)
   if (this._waiting.length) {
     var waiting = this._waiting.shift()
     waiting(item)
@@ -23,6 +28,7 @@ Queue.prototype.push = function(item) {
 
 Queue.prototype.pop = function(cb) { var self = this
   this.length--
+  this.emit('length-changed', this.length)
   if (this._items.length) {
     var item = this._items.shift()
     return Promise.resolve(item).nodeify(cb)
