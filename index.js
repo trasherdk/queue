98205a921cb60434cd6f8a6e8f8e83c5fad6c469
TrasherDK <trasherdk@fumlersoft.dk>
2022-02-03 13:52:28 +0100

    p42 refactoring - standard.js

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
-'use strict';
+'use strict'
 
 module.exports = ThenQueue
 module.exports.default = ThenQueue
 
-function ThenQueue() {
+function ThenQueue () {
   if (!(this instanceof ThenQueue)) return new ThenQueue()
-  this._items = new SimpleQueue();
-  this._waiting = new SimpleQueue();
+  this._items = new SimpleQueue()
+  this._waiting = new SimpleQueue()
   this.length = 0
 }
 
-ThenQueue.prototype.push = function push(item) {
+ThenQueue.prototype.push = function push (item) {
   this.length++
 
-  var waiting = this._waiting.shift()
+  const waiting = this._waiting.shift()
   if (waiting) {
     waiting(item)
-  }
-  else {
+  } else {
     this._items.push(item)
   }
 }
 
-ThenQueue.prototype.shift = function shift() {
+ThenQueue.prototype.shift = function shift () {
   this.length--
 
-  var item = this._items.shift()
+  const item = this._items.shift()
   if (item) {
     return Promise.resolve(item)
   }
-  else {
-    var waiting = this._waiting
-    return new Promise(function(resolve) {
-      waiting.push(resolve)
-    })
-  }
+  const waiting = this._waiting
+  return new Promise((resolve) => {
+    waiting.push(resolve)
+  })
 }
 
-function SimpleQueue() {
-  this._head = [];
-  this._tail = [];
+function SimpleQueue () {
+  this._head = []
+  this._tail = []
 }
-SimpleQueue.prototype.push = function push(item) {
-  this._tail.push(item);
+SimpleQueue.prototype.push = function push (item) {
+  this._tail.push(item)
 }
-SimpleQueue.prototype.shift = function shift() {
+SimpleQueue.prototype.shift = function shift () {
   if (this._head.length !== 0) {
-    return this._head.pop();
+    return this._head.pop()
   }
   if (this._tail.length === 1) {
-    return this._tail.pop();
+    return this._tail.pop()
   }
   if (this._tail.length > 1) {
-    var temp = this._tail.reverse()
+    const temp = this._tail.reverse()
     this._tail = this._head
     this._head = temp
-    return this._head.pop();
+    return this._head.pop()
   }
-  return undefined;
+  return undefined
 }
--- test/index.js
+++ test/index.js
@@ -1,50 +1,50 @@
-'use strict';
+'use strict'
 
-const assert = require('assert');
-const test = require('testit');
-const Queue = require('../');
+const assert = require('assert')
+const test = require('testit')
+const Queue = require('../')
 
 test('queue', () => {
-  const q = new Queue();
-  const results = [];
-  assert(q.length === 0);
-  q.push(1);
-  assert(q.length === 1);
-  q.push(2);
-  assert(q.length === 2);
-  q.push(3);
-  assert(q.length === 3);
+  const q = new Queue()
+  const results = []
+  assert(q.length === 0)
+  q.push(1)
+  assert(q.length === 1)
+  q.push(2)
+  assert(q.length === 2)
+  q.push(3)
+  assert(q.length === 3)
   return q.shift().then((n) => {
-    assert(n === 1);
-    assert(q.length === 2);
-    return q.shift();
+    assert(n === 1)
+    assert(q.length === 2)
+    return q.shift()
   }).then((n) => {
-    assert(n === 2);
-    assert(q.length === 1);
-    return q.shift();
+    assert(n === 2)
+    assert(q.length === 1)
+    return q.shift()
   }).then((n) => {
-    assert(n === 3);
-    assert(q.length === 0);
-    results.push(q.shift());
-    assert(q.length === -1);
-    results.push(q.shift());
-    assert(q.length === -2);
-    results.push(q.shift());
-    assert(q.length === -3);
-    q.push(1);
-    assert(q.length === -2);
-    return results.shift();
+    assert(n === 3)
+    assert(q.length === 0)
+    results.push(q.shift())
+    assert(q.length === -1)
+    results.push(q.shift())
+    assert(q.length === -2)
+    results.push(q.shift())
+    assert(q.length === -3)
+    q.push(1)
+    assert(q.length === -2)
+    return results.shift()
   }).then((n) => {
-    assert(n === 1);
-    q.push(2);
-    assert(q.length === -1);
-    return results.shift();
+    assert(n === 1)
+    q.push(2)
+    assert(q.length === -1)
+    return results.shift()
   }).then((n) => {
-    assert(n === 2);
-    q.push(3);
-    assert(q.length === 0);
-    return results.shift();
+    assert(n === 2)
+    q.push(3)
+    assert(q.length === 0)
+    return results.shift()
   }).then((n) => {
-    assert(n === 3);
-  });
-});
+    assert(n === 3)
+  })
+})
