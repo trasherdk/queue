3df57824b5f81889530afe4baf35522bf63814a6
TrasherDK <trasherdk@fumlersoft.dk>
2022-02-03 13:33:25 +0100

    type definitions - faster queue - rename pop to shift PR by @ForbesLindesay

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var assert = require('assert')
-var test = require('testit')
-var Queue = require('../')
+const assert = require('assert');
+const test = require('testit');
+const Queue = require('../');
 
-test('queue', function () {
-  var q = new Queue();
-  var results = [];
+test('queue', () => {
+  const q = new Queue();
+  const results = [];
   assert(q.length === 0);
   q.push(1);
   assert(q.length === 1);
@@ -14,15 +14,15 @@ test('queue', function () {
   assert(q.length === 2);
   q.push(3);
   assert(q.length === 3);
-  return q.shift().then(function (n) {
+  return q.shift().then((n) => {
     assert(n === 1);
     assert(q.length === 2);
     return q.shift();
-  }).then(function (n) {
+  }).then((n) => {
     assert(n === 2);
     assert(q.length === 1);
     return q.shift();
-  }).then(function (n) {
+  }).then((n) => {
     assert(n === 3);
     assert(q.length === 0);
     results.push(q.shift());
@@ -34,17 +34,17 @@ test('queue', function () {
     q.push(1);
     assert(q.length === -2);
     return results.shift();
-  }).then(function (n) {
+  }).then((n) => {
     assert(n === 1);
     q.push(2);
     assert(q.length === -1);
     return results.shift();
-  }).then(function (n) {
+  }).then((n) => {
     assert(n === 2);
     q.push(3);
     assert(q.length === 0);
     return results.shift();
-  }).then(function (n) {
+  }).then((n) => {
     assert(n === 3);
   });
 });
